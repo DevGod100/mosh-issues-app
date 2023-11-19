f92614d15237dcfd2a0786025a8865f56359e506
Steve Loewenstein <saloewenstein@my365.bellevue.edu>
2023-11-19 02:14:51 +0200

    Refactor: Apply the SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,34 +1,29 @@
-import IssuesStatusBadge from "@/app/Components/IssuesStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import ReactMarkdown from 'react-markdown'
-
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
 }
 const IssueDetailPage = async ({ params }: Props) => {
-  //   if (typeof params.id !== 'number') notFound();  //not working, makes all params fail...
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
   if (!issue) notFound();
 
-
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssuesStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt='4'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{initial: '1', md: '2'}} gap='5'>
+      <Box>
+     <IssueDetails issue={issue}/>
+      </Box>
+      <Box>
+       <EditIssueButton issueId={issue.id}/>
+      </Box>
+      
+    </Grid>
   );
 };
 
